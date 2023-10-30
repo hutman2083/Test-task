f2073c7d3f795a1e687e67aa45e208730656ecc8
hutman2083 <mr.2003sasha@mail.ru>
2023-10-30 14:44:40 +0300

    feat: add another form for the second button

--- index.js
+++ index.js
@@ -6,9 +6,21 @@ function showForm() {
 function hideForm() {
     document.getElementById('overlay').classList.add('hidden');
     document.getElementById('purchaseForm').classList.add('hidden');
+    document.getElementById('overlay1').classList.add('hidden');
+    document.getElementById('purchaseForm1').classList.add('hidden');
 }
 
 function submitForm() {
     console.log('Данные отправлены!');
     hideForm();
+}
+
+function submitForm1() {
+    console.log('Данные отправлены!');
+    hideForm();
+}
+
+function showForm1() {
+    document.getElementById('overlay1').classList.remove('hidden');
+    document.getElementById('purchaseForm1').classList.remove('hidden');
 }
